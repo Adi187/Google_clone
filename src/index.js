b9cab41013382ef0ff3cc49ea404bdf2c4961644
Aditya sharma <adityaraysharma@gmail.com>
2022-08-20 03:52:30 +0530

    added light mode and dark mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App';
+import './global.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+ReactDOM.render(
+ <Router>
+   <App />
+ </Router>, document.getElementById('root'))
