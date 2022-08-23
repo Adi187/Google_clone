cb56645a242d35705907130c4ae46980ab1802e8
Aditya sharma <adityaraysharma@gmail.com>
2022-08-23 16:27:19 +0530

    dark mode correction

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
           <Routes />
           <Footer />
         </div>
+       
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+
+import { Search } from './Search'
+
 export const Navbar = ({darkTheme, setDarkTheme}) => {
     return (
       <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
@@ -9,10 +12,11 @@ export const Navbar = ({darkTheme, setDarkTheme}) => {
                    Google🔍
                 </p>
             </Link>
-            <button type="button" onClick={()=> setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
-                {darkTheme ? '🔅Light': '🌙Dark'}
+            <button type="button" onClick={()=> setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-gray-900 border rounded-full px-2 py-1 hover:shadow-lg">
+                {darkTheme ? '🔅': '🌙Dark'}
             </button>
          </div>
+         <Search />
       </div>
     )
   }
